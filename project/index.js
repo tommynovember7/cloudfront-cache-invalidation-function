ac717a871a12aec40db6ac17097ebd2be94bc248
Tomo L. Miyamoto <lancelot@tommyheavenly7.com>
2021-06-06 10:18:33 +0000

    refactor: rename variable to hold cache invalidation items

--- project/index.js
+++ project/index.js
@@ -2,14 +2,11 @@ const { CloudFront } = require('aws-sdk');
 const createRequest = require('./src/InvalidationRequest').create;
 
 const { DISTRIBUTION_ID } = process.env;
-const VG_TARGET_ITEMS = [
-  '/horoscope/shiitake/',
-  '/horoscope/shiitake/*',
-];
+const CACHE_INVALIDATION_ITEMS = [];
 const cloudFront = new CloudFront();
 
 exports.handler = async (event) => {
-  const params = createRequest(DISTRIBUTION_ID, VG_TARGET_ITEMS);
+  const params = createRequest(DISTRIBUTION_ID, CACHE_INVALIDATION_ITEMS);
   console.log(event);
   console.log(params);
   await cloudFront.createInvalidation(params, (err, data) => {
